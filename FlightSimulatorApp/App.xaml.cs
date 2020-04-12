137087147a8208dfb984db356513ec7ac58d5c30
OmerMizrachi <omermizra@gmail.com>
2020-04-12 20:24:47 +0300

    Using App.config to get IP and Port if user set them.

--- FlightSimulatorApp/App.xaml.cs
+++ FlightSimulatorApp/App.xaml.cs
@@ -26,27 +26,43 @@ namespace FlightSimulatorApp {
             Regex ValidIPRegex =
                 new Regex(
                     "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
-            
-            /* View */
+
             MainWindow mainWindow = new MainWindow();
 
-            /* Model */
+            /* Instantiate the model:
+             If ip and port are provided in App.config parse them else use default values.*/
             SimulatorModel model = new SimulatorModel();
-            /* No Args (or not enough) args so use default values */
             model.Ip = DefaultIp;
             model.Port = DefaultPort;
             int port = -1;
-
-            /* Try to parse arguments into ip and port */
-            foreach (string curArg in e.Args) {
-                    if (ValidIPRegex.IsMatch(curArg)) {
-                        model.Ip = curArg;
-                    }
-                    else if (int.TryParse(curArg, out port) && port >= 0 && port <= 65535) {
-                        model.Port = port;
+            try {
+                var appSettings = ConfigurationManager.AppSettings;
+                if (appSettings.Count > 0) {
+                    foreach (var argKey in appSettings.AllKeys) {
+                        /* Parse ip and validate */
+                        if (argKey.ToLower().Contains("ip") && ValidIPRegex.IsMatch(appSettings[argKey])) {
+                            model.Ip = appSettings[argKey];
+                        }
+                        /* Parse port and validate */
+                        else if (argKey.ToLower().Contains("port") && int.TryParse(appSettings[argKey], out port) &&
+                                 port >= 0 && port <= 65535) {
+                            model.Port = port;
+                        }
                     }
                 }
-            
+            }
+            catch (ConfigurationErrorsException ce) {
+                // DO NOTHING
+            }
+            /* Try to parse arguments into ip and port */ /*
+            foreach (string curArg in e.Args) {
+                if (ValidIPRegex.IsMatch(curArg)) {
+                    model.Ip = curArg;
+                }
+                else if (int.TryParse(curArg, out port) && port >= 0 && port <= 65535) {
+                    model.Port = port;
+                }
+            }*/
 
             /* View Model */
             mainWindow.MyMap.SetVM(new MapVM(model));
